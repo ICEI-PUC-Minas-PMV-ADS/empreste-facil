928058600cc210b394fef3df49e4e01b4f88f04e
Iago Ferreira <iago-ferreira@outlook.com>
2022-09-10 20:43:29 -0300

    feat: add project base

---
+++ source/EmpresteFacil/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EmpresteFacil.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
