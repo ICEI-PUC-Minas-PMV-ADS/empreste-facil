2d100e064d2d65ffd5e81bb588e948ffe345ba9d
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-10-05 19:57:15 -0300

    Commit
    Add Endereco class

---
+++ source/EmpresteFacil/Models/Entities/Endereco.cs
@@ -0,0 +1,30 @@
+﻿namespace EmpresteFacil.Models.Entities
+{
+    class Endereco
+    {
+        public string TipoLogradouro { get; set; }
+        public string DescricaoLogradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Cep { get; set; }
+        public string Pais { get; set; }
+
+        public Endereco()
+        {
+        }
+
+        public Endereco(string tipoLogradouro, string descricaoLogradouro, string complemento, string bairro, string cidade, string estado, string cep, string pais)
+        {
+            TipoLogradouro = tipoLogradouro;
+            DescricaoLogradouro = descricaoLogradouro;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+            Cep = cep;
+            Pais = pais;
+        }
+    }
+}
