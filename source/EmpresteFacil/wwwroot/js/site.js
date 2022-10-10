50437f0e9c150734ee9557bee11a728f5388e63a
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-10-10 19:36:00 -0300

    Commit
    Update Js in Front-end

--- source/EmpresteFacil/wwwroot/js/site.js
+++ source/EmpresteFacil/wwwroot/js/site.js
@@ -4,5 +4,11 @@ var output = document.getElementById("valor-solicitado");
 output.innerHTML = slider.value;
 
 slider.oninput = function (){
-    output.innerHTML = this.value;
+    output.innerHTML = this.value * 500;
 }
+
+slider.addEventListener("mousemove", function () {
+    var x = slider.value;
+    var color = 'linear-gradient(90deg, rgb(117,252,117)' + x + '%, rgb(214,214,214)' + x + '%)';
+    slider.style.background = color;
+})
